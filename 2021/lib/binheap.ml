82bab0e6214dac9cb1859664afd90e8a1161cb40
Zack Marvel <zpmarvel@gmail.com>
2021-12-17 08:12:11 -0700

    Improve Binheap to store arbitrary value type (with priority)

--- 2021/lib/binheap.ml
+++ 2021/lib/binheap.ml
@@ -1,31 +1,51 @@
 
 
 type 'a t = {
-    a: 'a array;
+    a: 'a node array;
 
     (* "Size" is how many nodes are in the tree. "Capacity" is how many nodes can be held without
        resizing the array, and doesn't have to be maintained explicitly since the array has a
        "length" property. *)
     mutable size: int;
-};;
+}
+
+and 'a node =
+    Empty
+    | Node of (int * 'a) (* Node of (priority, value) -- should I use a record? *)
+;;
 
 
 let make capacity =
-    { a = Array.make capacity 0; size = 0 }
+    { a = Array.make capacity Empty; size = 0 }
 ;;
 
+
+exception Empty_binheap
+
+
+let get_priority heap i =
+    match heap.a.(i) with
+    | Node(prio, _) -> prio
+    | Empty -> raise Empty_binheap
+;;
 
 
 let rec heapify (heap : 'a t) (i : int) =
     let left = 2 * i + 1 in
     let right = 2 * i + 2 in
     let smallest = i in
+    let currprio = get_priority heap smallest in
     let smallest =
-        if left < heap.size && heap.a.(left) < heap.a.(smallest) then left
+        if left < heap.size then
+            let leftprio = get_priority heap left in
+            if leftprio < currprio then left else smallest
         else smallest
     in
+    let currprio = get_priority heap smallest in
     let smallest =
-        if right < heap.size && (heap.a.(right) < heap.a.(smallest)) then right
+        if right < heap.size then
+            let rightprio = get_priority heap right in
+            if rightprio < currprio then right else smallest
         else smallest
     in
     (* Printf.printf "i=%d left=%d right=%d smallest=%d\n" i left right smallest; *)
@@ -38,10 +58,13 @@ let rec heapify (heap : 'a t) (i : int) =
 ;;
 
 
-let make_from capacity a =
+let make_from capacity (a : (int * 'a) array) : 'a t =
     (* TODO: If capacity < Array.length a, raise *)
-    let a' = Array.make capacity 0 in
-    Array.blit a 0 a' 0 (Array.length a);
+    let a' = Array.make capacity Empty in
+    Array.iteri (fun i -> fun x ->
+        let prio, value = x in
+        a'.(i) <- Node(prio, value)
+    ) a;
     let heap = { a = a'; size = Array.length a } in
     let rec loop i = 
         (*
@@ -76,12 +99,15 @@ let empty heap =
 
 (* TODO: raise on empty, optional version *)
 let extract heap =
+    (* Remove the root node, swap it with the right-most leaf, and re-heapify *)
     let iswap = heap.size - 1 in
     let root = heap.a.(0) in
     heap.size <- heap.size - 1;
     heap.a.(0) <- heap.a.(iswap);
     heapify heap 0;
-    root
+    match root with
+    | Node(priority, value) -> (priority, value)
+    | Empty -> raise Empty_binheap
 ;;
 
 
@@ -90,17 +116,20 @@ let parent i =
 ;;
 
 
-let insert heap value =
+let insert heap priority value =
     if (size heap) = (capacity heap) then (* TODO: Resize the array *) ();
     let asize = heap.size in
-    heap.a.(asize) <- value;
+    heap.a.(asize) <- Node(priority, value);
     heap.size <- asize + 1;
     (* Printf.printf "size=%d size'=%d\n" asize heap.size; *)
+    (* Recursively walk up the tree until the heap property is restored *)
     let rec loop i =
         if i = 0 then ()
         else
             let iparent = parent i in
-            if heap.a.(i) < heap.a.(iparent)  then
+            let curr_prio = get_priority heap i in
+            let parent_prio = get_priority heap iparent in
+            if curr_prio < parent_prio then
             (* Swap the two nodes *)
             let tmp = heap.a.(i) in
             heap.a.(i) <- heap.a.(iparent);
--- 2021/test/binheap_test.ml
+++ 2021/test/binheap_test.ml
@@ -4,13 +4,13 @@ open OUnit2;;
 
 let test_insert _ =
     let heap = Binheap.make 16 in
-    Binheap.insert heap 1;
+    Binheap.insert heap 1 3;
     assert_equal (Binheap.capacity heap) 16;
     assert_equal (Binheap.size heap) 1;
     (* Didn't test that we actually put a "1" in the array, but we'll test that when we go to remove
      * it
      *)
-    Binheap.insert heap 2;
+    Binheap.insert heap 2 3;
     assert_equal (Binheap.capacity heap) 16;
     assert_equal (Binheap.size heap) 2;
 ;;
@@ -19,41 +19,47 @@ let test_insert _ =
 let test_extract _ =
     let heap = Binheap.make 16 in
 
-    Binheap.insert heap 1;
-    Binheap.insert heap 2;
-    assert_equal (Binheap.extract heap) 1;
-    assert_equal (Binheap.extract heap) 2;
+    Binheap.insert heap 1 3;
+    Binheap.insert heap 2 3;
+    assert_equal (Binheap.extract heap) (1, 3);
+    assert_equal (Binheap.extract heap) (2, 3);
     assert_equal (Binheap.empty heap) true;
 
-    Binheap.insert heap 2;
-    Binheap.insert heap 2;
-    Binheap.insert heap 1;
+    Binheap.insert heap 2 3;
+    Binheap.insert heap 2 3;
+    Binheap.insert heap 1 3;
     assert_equal (Binheap.size heap) 3;
-    assert_equal (Binheap.extract heap) 1;
-    assert_equal (Binheap.extract heap) 2;
-    assert_equal (Binheap.extract heap) 2;
+    assert_equal (Binheap.extract heap) (1, 3);
+    assert_equal (Binheap.extract heap) (2, 3);
+    assert_equal (Binheap.extract heap) (2, 3);
     assert_equal (Binheap.empty heap) true;
 ;;
 
 
-let test_make_from _ =
-    let a = [| 8; 5; 3; 4; 11; |] in
-    let heap = Binheap.make_from 16 a in
-    assert_equal (Binheap.size heap) 5;
-    assert_equal 3 (Binheap.extract heap) ~printer:string_of_int;
-    assert_equal 4 (Binheap.extract heap) ~printer:string_of_int;
-    assert_equal 5 (Binheap.extract heap) ~printer:string_of_int;
-    assert_equal 8 (Binheap.extract heap) ~printer:string_of_int;
-    assert_equal 11 (Binheap.extract heap) ~printer:string_of_int;
+let string_of_int_pair p =
+    let a, b = p in
+    Printf.sprintf "(%d, %d)" a b
+;;
 
-    let a = [| 11; 4; 3; 5; 8; |] in
+
+let test_make_from _ =
+    let a = [| (8, 3); (5, 3); (3, 3); (4, 3); (11, 3); |] in
     let heap = Binheap.make_from 16 a in
     assert_equal (Binheap.size heap) 5;
-    assert_equal 3 (Binheap.extract heap) ~printer:string_of_int;
-    assert_equal 4 (Binheap.extract heap) ~printer:string_of_int;
-    assert_equal 5 (Binheap.extract heap) ~printer:string_of_int;
-    assert_equal 8 (Binheap.extract heap) ~printer:string_of_int;
-    assert_equal 11 (Binheap.extract heap) ~printer:string_of_int;
+    assert_equal (3, 3) (Binheap.extract heap) ~printer:string_of_int_pair;
+    assert_equal (4, 3) (Binheap.extract heap) ~printer:string_of_int_pair;
+    assert_equal (5, 3) (Binheap.extract heap) ~printer:string_of_int_pair;
+    assert_equal (8, 3) (Binheap.extract heap) ~printer:string_of_int_pair;
+    assert_equal (11, 3) (Binheap.extract heap) ~printer:string_of_int_pair;
+
+    let a = [| (11, 5); (4, 5); (3, 5); (5, 5); (8, 5); |] in
+    let heap = Binheap.make_from 16 a in
+    assert_equal (Binheap.size heap) 5;
+    assert_equal (3, 5) (Binheap.extract heap) ~printer:string_of_int_pair;
+    assert_equal (4, 5) (Binheap.extract heap) ~printer:string_of_int_pair;
+    assert_equal (5, 5) (Binheap.extract heap) ~printer:string_of_int_pair;
+    assert_equal (8, 5) (Binheap.extract heap) ~printer:string_of_int_pair;
+    assert_equal (11, 5) (Binheap.extract heap) ~printer:string_of_int_pair;
 ;;
 
 

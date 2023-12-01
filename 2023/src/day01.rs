5c62fd6182bfea778f19552c31a97dea73f5c11b
Zack Marvel <zpmarvel@gmail.com>
2023-12-01 14:11:48 -0700

    run rustfmt

--- 2023/src/day01.rs
+++ 2023/src/day01.rs
@@ -1,18 +1,33 @@
 use std::string::String;
 
 pub fn extract_calibration_value(line: &String) -> i64 {
-    let first = line.find(char::is_numeric).expect("first calibration value");
-    let second = line.rfind(char::is_numeric).expect("second calibration value");
+    let first = line
+        .find(char::is_numeric)
+        .expect("first calibration value");
+    let second = line
+        .rfind(char::is_numeric)
+        .expect("second calibration value");
 
-    let first_num = line.get(first..(first+1)).unwrap().parse::<i64>().unwrap();
-    let second_num = line.get(second..(second+1)).unwrap().parse::<i64>().unwrap();
+    let first_num = line
+        .get(first..(first + 1))
+        .unwrap()
+        .parse::<i64>()
+        .unwrap();
+    let second_num = line
+        .get(second..(second + 1))
+        .unwrap()
+        .parse::<i64>()
+        .unwrap();
 
-    return first_num * 10 + second_num
+    return first_num * 10 + second_num;
 }
 
 // TODO: Refactor so this takes some sort of iterator? Or call the above helper in a loop.
 pub fn sum_calibration_values(lines: &[String]) -> i64 {
-    lines.iter().map(extract_calibration_value).fold(0, |acc, x| acc + x)
+    lines
+        .iter()
+        .map(extract_calibration_value)
+        .fold(0, |acc, x| acc + x)
 }
 
 #[cfg(test)]
@@ -27,7 +42,12 @@ mod tests {
 
     #[test]
     fn sum_calibration_values_full() {
-        let inputs = vec![String::from("1abc2"), String::from("pqr3stu8vwx"),String::from("a1b2c3d4e5f"),String::from("treb7uchet")];
+        let inputs = vec![
+            String::from("1abc2"),
+            String::from("pqr3stu8vwx"),
+            String::from("a1b2c3d4e5f"),
+            String::from("treb7uchet"),
+        ];
         assert_eq!(sum_calibration_values(&inputs), 142);
     }
 }
--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -1,8 +1,8 @@
 mod day01;
 
 use std::fs::File;
-use std::path::Path;
 use std::io::{self, BufRead};
+use std::path::Path;
 
 fn read_lines(file_path: &Path) -> io::Result<io::Lines<io::BufReader<File>>> {
     let file = File::open(file_path)?;

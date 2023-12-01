82a91ea6c22fa9399fecaff8c708a3b7654f1e9e
Zack Marvel <zpmarvel@gmail.com>
2023-12-01 13:43:54 -0700

    let's start 2023: day01

---
+++ 2023/src/main.rs
@@ -0,0 +1,26 @@
+mod day01;
+
+use std::fs::File;
+use std::path::Path;
+use std::io::{self, BufRead};
+
+fn read_lines(file_path: &Path) -> io::Result<io::Lines<io::BufReader<File>>> {
+    let file = File::open(file_path)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn day01() {
+    if let Ok(lines) = read_lines(Path::new("day01.input")) {
+        let mut result = 0;
+        for line in lines {
+            result += day01::extract_calibration_value(&line.unwrap());
+        }
+        println!("{}", result)
+    } else {
+        println!("Failed to open input")
+    }
+}
+
+fn main() {
+    day01();
+}
